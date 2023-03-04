154850601401df8366044d0e6a43e8d0def08c5d
yury mid <94337419+pieceowater@users.noreply.github.com>
2023-03-04 17:12:22 +0600

    Update crypt.js

--- crypt.js
+++ crypt.js
@@ -8,18 +8,18 @@ const base64_decode = (base64str) => {
     return Buffer.from(base64str, 'base64')//.toString('binary');//'binary'
 }
 
-export function sayHello() {
+function sayHello() {
   return "hello!";
 }
 
-export const mc_decrypt = (val) => {
+const mc_decrypt = (val) => {
     val = base64_decode(val.replaceAll('.','&').replaceAll('~','/')).toString('utf8')
     const [encrypted, iv] = val.split('::').map(e=>base64_decode(e));
     const decipher = crypto.createDecipheriv('aes-256-cbc', CRYPTO_KEY.toString('binary'), iv);
     return decipher.update(encrypted) + decipher.final('utf8');
 }
 
-export const mc_encrypt = (val) => {
+const mc_encrypt = (val) => {
     const iv = crypto.randomBytes(16);
     const cipher = crypto.createCipheriv('aes-256-cbc', CRYPTO_KEY.toString('binary'), iv);
     const encrypted = Buffer.concat([cipher.update(val, 'utf8'), cipher.final()]).toString('base64');
@@ -27,6 +27,8 @@ export const mc_encrypt = (val) => {
     return enc.replaceAll('&','.').replaceAll('/','~')
 }
 
-export const testencrypt = (req) => {
+const testencrypt = (req) => {
     return req.query.enc === "true" ? mc_encrypt(req.query.data) : mc_decrypt(req.query.data)
 }
+
+module.exports = {sayHello, mc_decrypt, mc_decrypt, mc_encrypt, testencrypt}
